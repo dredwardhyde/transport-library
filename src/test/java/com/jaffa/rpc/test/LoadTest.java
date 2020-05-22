3b4c476ba9ed9472247631767711953fefed2c04
komesergey <komesergey@gmail.com>
2020-05-22 09:29:42 +0300

    Now TestServer.java basically contains two tests: - Call itself via RPC - Start MainServer.java as separate JVM process and receive RPC calls from it

---
+++ src/test/java/com/jaffa/rpc/test/LoadTest.java
@@ -0,0 +1,68 @@
+package com.jaffa.rpc.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.RandomStringUtils;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@SuppressWarnings({"squid:S2187", "squid:S2925", "ConstantConditions"})
+public class LoadTest {
+    public static void main(String[] args) {
+        log.info("================ TEST SERVER STARTING ================");
+
+        System.setProperty("jaffa-rpc-config", "./jaffa-rpc-config-load-server.properties");
+
+        final AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(MainConfig.class);
+        ctx.refresh();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(ctx::close));
+
+        PersonServiceClient personService = ctx.getBean(PersonServiceClient.class);
+        ClientServiceClient clientService = ctx.getBean(ClientServiceClient.class);
+
+        // 1 hour load test
+        final boolean sync = true;
+        final boolean heavy = false;
+        Runnable runnable = () -> {
+            long startTime = System.currentTimeMillis();
+            while (!Thread.currentThread().isInterrupted() && (System.currentTimeMillis() - startTime) < (60 * 60 * 1000)) {
+                if (sync) {
+                    if (heavy) {
+                        personService.getHeavy(RandomStringUtils.randomAlphabetic(250_000))
+                                .onModule("load.server")
+                                .executeSync();
+                    } else {
+                        // Sync call
+                        clientService.lol3("test3")
+                                .onModule("load.server")
+                                .executeSync();
+                    }
+                } else {
+                    // Async call
+                    clientService.lol3("test3")
+                            .onModule("load.server")
+                            .withTimeout(10, TimeUnit.SECONDS)
+                            .executeAsync(UUID.randomUUID().toString(), ServiceCallback.class);
+                }
+                try {
+                    Thread.sleep((int) (Math.random() * 100));
+                } catch (InterruptedException exception) {
+                    exception.printStackTrace();
+                }
+            }
+        };
+        try {
+            Thread thread = new Thread(runnable);
+            thread.start();
+            thread.join();
+        } catch (Exception ignore) {
+        }
+
+        ctx.close();
+        System.exit(0);
+    }
+}
--- src/test/java/com/jaffa/rpc/test/TestServer.java
+++ src/test/java/com/jaffa/rpc/test/TestServer.java
@@ -1,9 +1,14 @@
 package com.jaffa.rpc.test;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.RandomStringUtils;
 import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import java.io.File;
 import java.util.Objects;
@@ -12,9 +17,15 @@ import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @SuppressWarnings("squid:S2187")
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {MainConfig.class}, loader = AnnotationConfigContextLoader.class)
 public class TestServer {
 
-    private static final boolean loadTest = false;
+    @Autowired
+    private PersonServiceClient personService;
+
+    @Autowired
+    ClientServiceClient clientService;
 
     private static String getClassPathFromParent() {
         return System.getProperty("java.class.path", "./*");
@@ -24,10 +35,9 @@ public class TestServer {
         return Objects.isNull(System.getProperty("java.home")) ? "java" : String.format("%s%sbin%sjava", System.getProperty("java.home"), File.separator, File.separator);
     }
 
-    public static void main(String... args) {
+    @Test
+    public void stage() {
         log.info("================ TEST SERVER STARTING ================");
-
-        System.setProperty("jaffa-rpc-config", "./jaffa-rpc-config-test-server.properties");
 
         final AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
         ctx.register(MainConfig.class);
@@ -38,84 +48,44 @@ public class TestServer {
         PersonServiceClient personService = ctx.getBean(PersonServiceClient.class);
         ClientServiceClient clientService = ctx.getBean(ClientServiceClient.class);
 
-        if (!loadTest) {
-            Integer id = personService.add("Test name", "test@mail.com", null)
-                    .withTimeout(15, TimeUnit.SECONDS)
+        Integer id = personService.add("Test name", "test@mail.com", null)
+                .withTimeout(15, TimeUnit.SECONDS)
+                .onModule("test.server")
+                .executeSync();
+        log.info("Resulting id is {}", id);
+        Person person = personService.get(id)
+                .onModule("test.server")
+                .executeSync();
+        Assert.assertEquals(person.getId(), id);
+        log.info(person.toString());
+        personService.lol().executeSync();
+        personService.lol2("kek").executeSync();
+        String name = personService.getName().executeSync();
+        log.info("Name: {}", name);
+        Assert.assertNull(name);
+        clientService.lol3("test3")
+                .onModule("test.server")
+                .executeSync();
+        clientService.lol4("test4")
+                .onModule("test.server")
+                .executeSync();
+        clientService.lol4("test4")
+                .onModule("test.server")
+                .withTimeout(10, TimeUnit.SECONDS)
+                .executeAsync(UUID.randomUUID().toString(), ServiceCallback.class);
+        personService.get(id)
+                .onModule("test.server")
+                .executeAsync(UUID.randomUUID().toString(), PersonCallback.class);
+        personService.lol2("kek").executeSync();
+        try {
+            personService.testError()
                     .onModule("test.server")
                     .executeSync();
-            log.info("Resulting id is {}", id);
-            Person person = personService.get(id)
-                    .onModule("test.server")
-                    .executeSync();
-            Assert.assertEquals(person.getId(), id);
-            log.info(person.toString());
-            personService.lol().executeSync();
-            personService.lol2("kek").executeSync();
-            String name = personService.getName().executeSync();
-            log.info("Name: {}", name);
-            Assert.assertNull(name);
-            clientService.lol3("test3")
-                    .onModule("test.server")
-                    .executeSync();
-            clientService.lol4("test4")
-                    .onModule("test.server")
-                    .executeSync();
-            clientService.lol4("test4")
-                    .onModule("test.server")
-                    .withTimeout(10, TimeUnit.SECONDS)
-                    .executeAsync(UUID.randomUUID().toString(), ServiceCallback.class);
-            personService.get(id)
-                    .onModule("test.server")
-                    .executeAsync(UUID.randomUUID().toString(), PersonCallback.class);
-            personService.lol2("kek").executeSync();
-            try {
-                personService.testError()
-                        .onModule("test.server")
-                        .executeSync();
-            } catch (Throwable e) {
-                log.error("Exception during sync call:", e);
-                Assert.assertTrue(e.getMessage().contains("very bad in") || (e.getCause() != null && e.getCause().getMessage().contains("very bad in")));
-            }
-            personService.testError().onModule("test.server").executeAsync(UUID.randomUUID().toString(), PersonCallback.class);
-        } else {
-            // 1 hour load test
-            final boolean sync = true;
-            final boolean heavy = false;
-            Runnable runnable = () -> {
-                long startTime = System.currentTimeMillis();
-                while (!Thread.currentThread().isInterrupted() && (System.currentTimeMillis() - startTime) < (60 * 60 * 1000)) {
-                    if (sync) {
-                        if (heavy) {
-                            personService.getHeavy(RandomStringUtils.randomAlphabetic(250_000))
-                                    .onModule("test.server")
-                                    .executeSync();
-                        } else {
-                            // Sync call
-                            clientService.lol3("test3")
-                                    .onModule("test.server")
-                                    .executeSync();
-                        }
-                    } else {
-                        // Async call
-                        clientService.lol3("test3")
-                                .onModule("test.server")
-                                .withTimeout(10, TimeUnit.SECONDS)
-                                .executeAsync(UUID.randomUUID().toString(), ServiceCallback.class);
-                    }
-                    try {
-                        Thread.sleep((int) (Math.random() * 100));
-                    } catch (InterruptedException exception) {
-                        exception.printStackTrace();
-                    }
-                }
-            };
-            try {
-                Thread thread = new Thread(runnable);
-                thread.start();
-                thread.join();
-            } catch (Exception ignore) {
-            }
+        } catch (Throwable e) {
+            log.error("Exception during sync call:", e);
+            Assert.assertTrue(e.getMessage().contains("very bad in") || (e.getCause() != null && e.getCause().getMessage().contains("very bad in")));
         }
+        personService.testError().onModule("test.server").executeAsync(UUID.randomUUID().toString(), PersonCallback.class);
 
         final String javaCmd = getJavaCmdFromParent();
         final String classpath = getClassPathFromParent();
@@ -126,10 +96,9 @@ public class TestServer {
             Process process = proc.start();
             int returnCode = process.waitFor();
             log.info("Main test server returned {}", returnCode);
+            Assert.assertEquals(0, returnCode);
         } catch (Exception e) {
             log.error("Exception while launching main.server", e);
         }
-        ctx.close();
-        System.exit(0);
     }
 }
