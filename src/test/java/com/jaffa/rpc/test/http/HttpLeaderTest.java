472b2c3ce70dc58d5ed520aa871e91677b92aed0
edwardhyde <komesergey@gmail.com>
2020-07-20 01:04:27 +0300

    Added HTTPS test

--- src/test/java/com/jaffa/rpc/test/http/HttpLeaderTest.java
+++ src/test/java/com/jaffa/rpc/test/http/HttpLeaderTest.java
@@ -11,11 +11,11 @@ public class HttpLeaderTest extends AbstractLeaderTestServer {
         System.setProperty("jaffa.rpc.protocol.use.https", "true");
         System.setProperty("jaffa.rpc.protocol.http.ssl.server.truststore.location", "src/test/resources/truststore.jks");
         System.setProperty("jaffa.rpc.protocol.http.ssl.server.truststore.password", "simulator");
-        System.setProperty("jaffa.rpc.protocol.http.ssl.server.keystore.location", "src/test/resources/identity.jks");
+        System.setProperty("jaffa.rpc.protocol.http.ssl.server.keystore.location", "src/test/resources/keystore.jks");
         System.setProperty("jaffa.rpc.protocol.http.ssl.server.keystore.password", "simulator");
         System.setProperty("jaffa.rpc.protocol.http.ssl.client.truststore.location", "src/test/resources/truststore.jks");
         System.setProperty("jaffa.rpc.protocol.http.ssl.client.truststore.password", "simulator");
-        System.setProperty("jaffa.rpc.protocol.http.ssl.client.keystore.location", "src/test/resources/identity.jks");
+        System.setProperty("jaffa.rpc.protocol.http.ssl.client.keystore.location", "src/test/resources/keystore.jks");
         System.setProperty("jaffa.rpc.protocol.http.ssl.client.keystore.password", "simulator");
     }
 
