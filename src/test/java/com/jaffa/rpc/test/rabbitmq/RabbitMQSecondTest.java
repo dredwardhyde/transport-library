72428c74239614a8f2ebd2873564ad0279d201c9
edwardhyde <komesergey@gmail.com>
2021-07-01 18:07:28 +0300

    Small refactoring

--- src/test/java/com/jaffa/rpc/test/rabbitmq/RabbitMQSecondTest.java
+++ src/test/java/com/jaffa/rpc/test/rabbitmq/RabbitMQSecondTest.java
@@ -51,7 +51,7 @@ public class RabbitMQSecondTest {
         try {
             rabbitMQRequestSender.executeAsync(new byte[]{});
             fail();
-        } catch (JaffaRpcNoRouteException JaffaRpcNoRouteException) {
+        } catch (JaffaRpcExecutionException JaffaRpcNoRouteException) {
             //No-op
         }
         try {
